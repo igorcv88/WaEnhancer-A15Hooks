f400907b6273404d6ebc075ee896f33b72afaa98
Igor <igorv88@gmail.com>
2025-08-03 05:44:57 -0300

    Patch.java

--- app/src/main/java/com/wmods/wppenhacer/xposed/downgrade/Patch.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/downgrade/Patch.java
@@ -1,8 +1,4 @@
-
 package com.wmods.wppenhacer.xposed.downgrade;
-
-import static de.robv.android.xposed.XposedBridge.hookMethod;
-import static de.robv.android.xposed.XposedHelpers.findMethodExactIfExists;
 
 import android.os.Build;
 
@@ -11,18 +7,21 @@ import com.wmods.wppenhacer.xposed.core.FeatureLoader;
 import java.lang.reflect.Method;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XSharedPreferences;
 
-public class Patch implements IXposedHookLoadPackage {
+public class Patch {
 
-    @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public static void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam, final XSharedPreferences prefs) {
 
         if (!lpparam.packageName.equals("android")) return;
 
-        if (!FeatureLoader.getBoolean("downgrade", true)) return;
+        if (!prefs.getBoolean("downgrade", true)) return;
 
-        Object hookDowngradeObject = new de.robv.android.xposed.XC_MethodHook() {
+        XC_MethodHook hookDowngradeObject = new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 param.setResult(null); // bypass downgrade check
@@ -33,21 +32,19 @@ public class Patch implements IXposedHookLoadPackage {
             case Build.VERSION_CODES.S: // Android 12
             case Build.VERSION_CODES.S_V2: // Android 12L
             case Build.VERSION_CODES.TIRAMISU: // Android 13
-                hookMethod(
-                    findMethodExactIfExists(
-                        "com.android.server.pm.PackageManagerServiceUtils",
-                        lpparam.classLoader,
-                        "checkDowngrade",
-                        "com.android.server.pm.parsing.pkg.ParsedPackage",
-                        "android.content.pm.PackageInfoLite"
-                    ),
+                XposedHelpers.findAndHookMethod(
+                    "com.android.server.pm.PackageManagerServiceUtils",
+                    lpparam.classLoader,
+                    "checkDowngrade",
+                    "com.android.server.pm.parsing.pkg.ParsedPackage",
+                    "android.content.pm.PackageInfoLite",
                     hookDowngradeObject
                 );
                 break;
 
             case Build.VERSION_CODES.UPSIDE_DOWN_CAKE: // Android 14
             case Build.VERSION_CODES.VANILLA_ICE_CREAM: // Android 15
-                Method checkMethod = findMethodExactIfExists(
+                Method checkMethod = XposedHelpers.findMethodExactIfExists(
                     "com.android.server.pm.PackageManagerServiceUtils",
                     lpparam.classLoader,
                     "checkDowngrade",
@@ -55,9 +52,9 @@ public class Patch implements IXposedHookLoadPackage {
                     "android.content.pm.PackageInfoLite"
                 );
                 if (checkMethod != null) {
-                    hookMethod(checkMethod, hookDowngradeObject);
+                    XposedBridge.hookMethod(checkMethod, hookDowngradeObject);
                 } else {
-                    de.robv.android.xposed.XposedBridge.log("W/WppEnhancer: checkDowngrade not found on Android 14+");
+                    XposedBridge.log("W/WppEnhancer: checkDowngrade not found on Android 14+");
                 }
                 break;
         }
